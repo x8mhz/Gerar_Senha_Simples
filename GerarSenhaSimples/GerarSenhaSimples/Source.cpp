7bed93d8815a4b6aa8919d646bd8237ce2383a15
Fabricio Ramos <fabricio.ramoss@hotmail.com>
2016-09-20 00:56:36 -0300

    Gerador de Senha Simples

---
+++ GerarSenhaSimples/GerarSenhaSimples/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+static const char caracteres_Possiveis[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+										   "abcdefghijklmnopqrstuvwxyz"
+										   "0123456789"
+										   "!@#$%^&*_";
+int quant_Caracteres = sizeof(caracteres_Possiveis) - 1;
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+	int tam_Senha;
+	cout << " --------------------------------" << endl;
+	cout << " |      GERADOR DE SENHA 5000   |" << endl;
+	cout << " --------------------------------" << endl << endl;
+	cout << " TAMANHO DA SENHA: ";
+	cin >> tam_Senha;
+
+	srand(time(0));
+	cout << endl << " SENHA GERADA É: ";
+	for (int i = 0; i < tam_Senha; i++)
+	{
+		cout << caracteres_Possiveis[rand() % quant_Caracteres];
+	}
+
+	cout << endl << endl;
+	system("PAUSE");
+	return 0;
+}
